f3e33a52b45834766d79c4dd847bb8f8e27650b2
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-05 20:30:48 -0300

    tsc

---
+++ Modulo D/aula11/ex008.js
@@ -0,0 +1,7 @@
+let vel = 90
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+if (vel > 60){  // Condição Simples!
+    console.log(`Você excedeu o limite máximo de velocidade permitido que era de 60 KM/H e será MULTADO!!!`)
+}
+console.log("Dirija sempre usando o cinto de segurança!")
+
