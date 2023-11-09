d5fe0bc9a0eb9eef8bf676cfd3ea3d0bb06e4272
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-09 00:34:40 -0300

    aa

--- Modulo D/exercicios/ex1/script.js
+++ Modulo D/exercicios/ex1/script.js
@@ -1,1 +1,9 @@
-alert('ola')
+let agora = new Date()
+let hora = agora.getHours()
+let horamin = agora.getMinutes()
+let horaseg = agora.getSeconds()
+
+let resultado = document.querySelector('div.resultado')
+resultado.innerHTML = `Agora são : ${hora}:${horamin}:${horaseg} horas`
+
+
