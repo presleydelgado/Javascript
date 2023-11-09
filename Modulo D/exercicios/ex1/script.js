720f5a3f3fef879da2691297a5217b4bfac2fb5d
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-09 17:36:51 -0300

    aaa

--- Modulo D/exercicios/ex1/script.js
+++ Modulo D/exercicios/ex1/script.js
@@ -6,6 +6,15 @@ let horaseg = agora.getSeconds()
 
 
 let resultado = document.querySelector('div.resultado')
+horamin = checkTime(horamin);
+horaseg = checkTime(horaseg);
 resultado.innerHTML = `Agora são : ${hora}:${horamin}:${horaseg} horas`
 setTimeout(startTime,1000);
 }
+//daqui pra baixo eu copiei,para dar o charme de fazer o relogio contar as horas continuamente
+function checkTime(segundos) {
+    if (segundos < 10) {
+        segundos = "0" + segundos
+    };  // add zero in front of numbers < 10
+    return segundos;
+  }
