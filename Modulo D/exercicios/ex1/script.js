e703081a03057118d578ac74fe53896c7d406c3c
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-09 16:48:46 -0300

    aaa

--- Modulo D/exercicios/ex1/script.js
+++ Modulo D/exercicios/ex1/script.js
@@ -3,7 +3,6 @@ let hora = agora.getHours()
 let horamin = agora.getMinutes()
 let horaseg = agora.getSeconds()
 
+
 let resultado = document.querySelector('div.resultado')
 resultado.innerHTML = `Agora são : ${hora}:${horamin}:${horaseg} horas`
-
-
