375a36a9053b32124874843db8590d54529ca674
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-11 00:45:44 -0300

    agora sim

--- Modulo D/exercicios/ex1/script.js
+++ Modulo D/exercicios/ex1/script.js
@@ -1,4 +1,5 @@
 function startTime(){
+let img = window.document.getElementById('imagem')
 let agora = new Date()
 let hora = agora.getHours()
 let horamin = agora.getMinutes()
@@ -9,6 +10,13 @@ horaseg = checkTime(horaseg);
 resultado.innerHTML = `Agora são : ${hora}:${horamin}:${horaseg} horas`
 setTimeout(startTime,1000);
 
+if(hora >= 0 && hora < 12){
+    img.src = 'foto 136.jpg'
+}else if (hora >= 12 && hora < 18){
+    img.src = 'foto 110.gif'
+}else {
+    img.src = 'foto 134.jpg'
+}
 
 }
 //daqui pra baixo eu copiei,para dar o charme de fazer o relogio contar as horas continuamente
