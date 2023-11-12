c1b8922575513df5bdc5c635fa1972b12e2bd504
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-12 17:11:30 -0300

    mulher pronto

--- Modulo D/exercicios/ex2/script.js
+++ Modulo D/exercicios/ex2/script.js
@@ -15,30 +15,34 @@ function verificar(){
         if (fsex[0].checked){
             genero = 'Homem'
             switch (true){
-                case idade <= 14 :
+                case idade < 18 :
                     img.src = "imagens/m1.gif"
                     break;
-                case idade >= 30 :
+                case idade < 30 :
                     img.src = "imagens/m2.png"
                     break;
                 case idade > 50 :
-                    img.src = "imagens/m4.jpg"
+                    img.src = "imagens/m3.jpg"
                 break;
             }
             img.style.display = 'block';
         } else if (fsex[1].checked){
             genero = 'Mulher'
             switch (true){
-                case idade <= 14 :
-                    img.src = "imagens/f1.jpg"
+                case idade < 12 :
+                    img.src = "imagens/f1.png"
                     break;
-                case idade >= 30 :
-                    img.src = "imagens/f3.jpg"
+                case idade < 25 :
+                    img.src = "imagens/f2.png"
+                    break;
+                case idade <= 40 :
+                    img.src = "imagens/f3.png"
                     break;
                 case idade > 50 :
-                    img.src = "imagens/f4.gif"
+                    img.src = "imagens/f4.png"
                 break;
             }
+            img.style.display = 'block';
         }
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
 
