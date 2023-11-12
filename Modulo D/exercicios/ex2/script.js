e17320c90cd6a0555ed55ce67173a13bf49ebcdc
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-12 00:11:53 -0300

    asdasdsa

--- Modulo D/exercicios/ex2/script.js
+++ Modulo D/exercicios/ex2/script.js
@@ -1,3 +1,14 @@
+
 function verificar(){
-    
+    window.alert("FUNCIONOU")
+    let idade = document.getElementById('anonasc')
+    let masc = document.getElementById('masculino')
+    let fem = document.getElementById('feminino')
+    let res = document.getElementById('resultado')
+
+    if(masc == masculino){
+        res.innerHTML = `aaaa ${masc}`    
+    }else if (fem){
+        res.innerHTML = `aaaa ${fem}`
+    }
 }
