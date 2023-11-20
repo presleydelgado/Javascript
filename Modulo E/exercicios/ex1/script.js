d187bce492711ab588c89c5e0d413cdd7c42beec
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-20 19:31:08 -0300

    aaa

--- Modulo E/exercicios/ex1/script.js
+++ Modulo E/exercicios/ex1/script.js
@@ -1,3 +1,11 @@
 function contar(){
-    alert('funcionou')
+    //alert('funcionou')
+    let inicio = document.getElementById('inicio').value;
+    let fim = document.getElementById('fim').value;
+    let passo = document.getElementById('passo').value;
+    
+    
+    
+   
+    }
 }
