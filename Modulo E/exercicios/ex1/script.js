9770535863867b6d751fd06e26b0abbea8230982
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-20 21:05:25 -0300

    oi

--- Modulo E/exercicios/ex1/script.js
+++ Modulo E/exercicios/ex1/script.js
@@ -3,14 +3,14 @@ function contar(){
     let inicio = document.getElementById('inicio').value;
     let fim = document.getElementById('fim').value;
     let passo = document.getElementById('passo').value;
-    
+    let soma;
     
     
    
     //tem que pegar o inicio , comparar com o valor final
     //pegar o inicio e somar com o passo até chegar no valor final...
 
-    if(inicio < fim){
+/*    if(inicio < fim){
         do {
             inicio = (inicio + passo)
             inicio++
@@ -18,4 +18,13 @@ function contar(){
         }while(inicio == fim)
         alert(`${inicio}`)
     }
+*/
+    if(inicio < fim){
+        do{
+            soma = inicio+passo
+            inicio++
+            alert(`${inicio}`)
+        }while(inicio < fim)
+    }
+
 }
