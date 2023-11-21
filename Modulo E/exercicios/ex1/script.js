ed8c53fd93a7a69f081935d0f65475aa03b0934e
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-20 21:39:42 -0300

    Feito uma parte do script

--- Modulo E/exercicios/ex1/script.js
+++ Modulo E/exercicios/ex1/script.js
@@ -1,12 +1,24 @@
 function contar(){
     //alert('funcionou')
-    let inicio = document.getElementById('inicio').value;
-    let fim = document.getElementById('fim').value;
-    let passo = document.getElementById('passo').value;
-    let soma;
-    
-    
+    let inicio = document.getElementById('inicio').valueAsNumber;
+    let fim = document.getElementById('fim').valueAsNumber;
+    let passo = document.getElementById('passo').valueAsNumber;   //O segredo tava em por .valueasnumber,se não ele não soma os valores
    
+    
+
+    while(inicio <= fim){
+        if(inicio + passo >= fim){
+            alert(`Fim da operação !`)
+            break;
+        }
+        inicio += passo;
+        alert(`${inicio}`)
+        
+        }
+        }
+    
+    
+
     //tem que pegar o inicio , comparar com o valor final
     //pegar o inicio e somar com o passo até chegar no valor final...
 
@@ -19,12 +31,19 @@ function contar(){
         alert(`${inicio}`)
     }
 */
+
+/*
     if(inicio < fim){
         do{
-            soma = inicio+passo
-            inicio++
-            alert(`${inicio}`)
-        }while(inicio < fim)
+            soma = (inicio + passo)
+            conta = (soma + inicio)
+            conta++
+            
+            alert(`${conta}`)
+        }while(conta < fim)
     }
 
 }
+
+*/
+
