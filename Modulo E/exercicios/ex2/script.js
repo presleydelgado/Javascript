ce9581e2c5467add7e4833ad9e417a48665ee86b
Presley Bonifacio Delgado <presley.bonifacio@gmail.com>
2023-11-22 19:32:44 -0300

    ex 2 pronto

---
+++ Modulo E/exercicios/ex2/script.js
@@ -0,0 +1,22 @@
+function gerar(){
+    let numero = document.getElementById('numero').value;
+    let resultado = document.getElementById('resultado');
+    let y = 0;
+
+    resultado.innerHTML = ' ';
+
+    for(numero; numero < 10; numero++)
+    {      
+        for(y; y < 10; y++)
+        {
+            resultado.innerHTML += `${numero} X ${y} = ${numero*y} <br>`
+        }
+        
+        if(numero === "")
+        {
+            alert('Valor invalido! Por favor,digite um número')
+            resultado.innerHTML = ' ';
+            break;
+        }
+    }
+}
